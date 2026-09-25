d74274fa3aa1cde16bf877096c09d5ae34b9ac60
AndresRendonS <andres.rendon@nuevaeps.com.co>
2026-09-25 09:16:54 -0500

    Clean up imports for Spring Boot build

--- backend/src/main/java/co/com/nuevaeps/eventos/EventController.java
+++ backend/src/main/java/co/com/nuevaeps/eventos/EventController.java
@@ -5,7 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDateTime;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
